e5fad186014cd9dfaedc74c24ecb80441a489950
Mateusz Rajski <mateusz.rajski@swmansion.com>
2025-03-11 14:02:55 +0100

    Add script for automated publishing

---
+++ scripts/publish-react-native.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Read react-native version from package.json
+REACT_NATIVE_VERSION=$(jq -r '.dependencies["react-native"]' package.json)
+
+# Create a temporary directory for the react-native repo
+TEMP_REACT_NATIVE_DIR=$(mktemp -d ./tmp-react-native-repo)
+
+echo "Cloning react-native repo to $TEMP_REACT_NATIVE_DIR"
+git clone --branch "v$REACT_NATIVE_VERSION" --single-branch --depth 1 https://github.com/facebook/react-native.git $TEMP_REACT_NATIVE_DIR
+
+# Use our patched ReactAndroid from node_modules
+cp -r ./node_modules/react-native/ReactAndroid $TEMP_REACT_NATIVE_DIR/packages/react-native/
+
+cd $TEMP_REACT_NATIVE_DIR
+
+# Install dependencies
+yarn install
+
+# Build and publish to maven repository
+./gradlew build
+./gradlew publishAllToMavenTempLocal
+
+# Remove a temporary directory
+rm -rf $TEMP_REACT_NATIVE_DIR
