5ec5d470712dddbd4df94fd23c88aa5e746c7ac5
Mateusz Rajski <mateusz.rajski@swmansion.com>
2025-03-12 13:27:35 +0100

    Update workflow

--- scripts/publish-react-native.sh
+++ scripts/publish-react-native.sh
@@ -6,7 +6,7 @@ REACT_NATIVE_VERSION=$(jq -r '.dependencies["react-native"]' package.json)
 # Create a temporary directory for the react-native repo
 TEMP_REACT_NATIVE_DIR=$(mktemp -d ./tmp-react-native-repo)
 
-echo "Cloning react-native repo to $TEMP_REACT_NATIVE_DIR"
+echo "Cloning react-native (v$REACT_NATIVE_VERSION) to $TEMP_REACT_NATIVE_DIR"
 git clone --branch "v$REACT_NATIVE_VERSION" --single-branch --depth 1 https://github.com/facebook/react-native.git $TEMP_REACT_NATIVE_DIR
 
 # Use our patched ReactAndroid from node_modules
@@ -15,7 +15,7 @@ cp -r ./node_modules/react-native/ReactAndroid $TEMP_REACT_NATIVE_DIR/packages/r
 cd $TEMP_REACT_NATIVE_DIR
 
 # Install dependencies
-yarn install
+yarn install --non-interactive
 
 # Build and publish to maven repository
 ./gradlew build
