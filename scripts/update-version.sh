6e1bdddb4ea2cef820e9382af155558ff683e1dc
Mateusz Rajski <60450261+mateuuszzzzz@users.noreply.github.com>
2025-03-24 13:40:28 +0100

    Setup local environment (#6)
    * compute hash
    
    * add ls
    
    * block the build artifacts action
    
    * change path
    
    * check if version exists
    
    * test
    
    * test
    
    * fail on unmatched hash
    
    * add random log to change patches
    
    * wip
    
    * move testing code
    
    * remove fetch
    
    * set gh token
    
    * use correct value
    
    * fix json file
    
    * update json file
    
    * wip
    
    * wip
    
    * Add script for automated publishing
    
    * Setup app to use our artifacts
    
    * Set our publishing group
    
    * Remove redundant comments
    
    * Fix publishing patches
    
    * Update patches
    
    * Update workflow
    
    * Update workflow
    
    * Remove yarn cache
    
    * Update workflow
    
    * Add more X to mktemp
    
    * Use docker image
    
    * Remove redundant checkout
    
    * Read version from previous job
    
    * Build for one architecture
    
    * Exclude redundant tasks
    
    * Exclude more tasks to make testing faster
    
    * Update patch
    
    * Add token to publish artifacts
    
    * Apply patches
    
    * Update patches
    
    * Update workflow
    
    * update patchlgo
    
    * Improve workflow readability
    
    * Add debug echos
    
    * Fix typo
    
    * Update version incrementation
    
    * Set version env
    
    * Cover all cases
    
    * Update mappings
    
    * fix
    
    * Checkout repository
    
    * Use ref main
    
    * Handle case when mappings are not created
    
    * Fix typo
    
    * setup local environment
    
    * Setup local env
    
    ---------
    
    Co-authored-by: war-in <war-in@users.noreply.github.com>

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -2,14 +2,21 @@
 
 PROPERTIES_FILE="node_modules/react-native/ReactAndroid/gradle.properties"
 
-# Get version from environment variable or fallback version from package.json (used for builds from source)
-if [ -n "$RN_VERSION" ]; then
-    CURRENT_VERSION="$RN_VERSION"
-else
-    CURRENT_VERSION=$(jq -r '.dependencies["react-native"]' package.json)
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to read version from package.json"
-        exit 1
+# Get RN version from package.json
+CURRENT_VERSION=$(jq -r '.dependencies["react-native"]' package.json)
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to read react-native version from package.json"
+    exit 1
+fi
+
+# Compute hash of all patches for given RN version
+HASH=$(find patches -type f -name "react-native+${CURRENT_VERSION}*.patch" -exec sha256sum {} + | sort | sha256sum | awk '{print $1}')
+
+if [ -f "mappings.json" ]; then
+    MAPPED_VERSION=$(jq -r --arg hash "$HASH" '.[$hash] // empty' mappings.json)
+    if [ ! -z "$MAPPED_VERSION" ]; then
+        echo "Found mapped version: $CURRENT_VERSION for hash: $HASH"
+        CURRENT_VERSION="$MAPPED_VERSION"
     fi
 fi
 
